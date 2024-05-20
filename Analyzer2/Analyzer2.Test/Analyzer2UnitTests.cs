61b99bfd9feebaab68f88eb9f2321a0943cd85d4
Sudqi Jawabreh <sudqiomar@gmail.com>
2024-05-20 11:31:09 +0300

    Add project files.

---
+++ Analyzer2/Analyzer2.Test/Analyzer2UnitTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = Analyzer2.Test.CSharpCodeFixVerifier<
+    Analyzer2.AwaitInLoopAnalyzer,
+    Analyzer2.Analyzer2CodeFixProvider>;
+
+namespace Analyzer2.Test
+{
+    [TestClass]
+    public class Analyzer2UnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class {|#0:TypeName|}
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("Analyzer2").WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
---
+++ Analyzer2/Analyzer2/AwaitInLoopAnalyzer.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+namespace Analyzer2
+{
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AwaitInLoopAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AwaitInLoop";
+        private static readonly LocalizableString Title = "Avoid 'await' inside loops";
+        private static readonly LocalizableString MessageFormat = "'await' call inside {0} loop";
+        private static readonly LocalizableString Description = "Asynchronous method calls should not be awaited inside loops.";
+        private const string Category = "Performance";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ForStatement, SyntaxKind.ForEachStatement, SyntaxKind.WhileStatement, SyntaxKind.DoStatement);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var loopStatement = (StatementSyntax)context.Node;
+
+            foreach (var descendantNode in loopStatement.DescendantNodes())
+            {
+                if (descendantNode is AwaitExpressionSyntax awaitExpression)
+                {
+                    var diagnostic = Diagnostic.Create(Rule, awaitExpression.GetLocation(), loopStatement.Kind().ToString());
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
